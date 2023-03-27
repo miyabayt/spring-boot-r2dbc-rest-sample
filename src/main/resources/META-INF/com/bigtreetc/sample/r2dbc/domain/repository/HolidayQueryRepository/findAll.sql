90e21579d4c53cc3b88fcae33a09bad1982671ad
miyabayt <4364793+miyabayt@users.noreply.github.com>
2023-03-27 12:37:53 +0900

    feat: 検索条件に日付を指定できるようにする

--- src/main/resources/META-INF/com/bigtreetc/sample/r2dbc/domain/repository/HolidayQueryRepository/findAll.sql
+++ src/main/resources/META-INF/com/bigtreetc/sample/r2dbc/domain/repository/HolidayQueryRepository/findAll.sql
@@ -10,6 +10,9 @@ WHERE
 /*%if criteria.holidayName != null */
     AND h.holiday_name LIKE /* @infix(criteria.holidayName) */'john'
 /*%end*/
+/*%if criteria.holidayDate != null */
+    AND h.holiday_date = /* criteria.holidayDate */'2000-01-01'
+/*%end*/
 ORDER BY
     h.holiday_date asc
     , h.holiday_name ASC
